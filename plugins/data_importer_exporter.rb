bed8ed721d2a0757785a257b2ba9e4a1e2d0b942
Peter Lauck <griest024@gmail.com>
2015-04-22 14:50:17 -0400

    merged common

--- plugins/data_importer_exporter.rb
+++ plugins/data_importer_exporter.rb
@@ -35,7 +35,7 @@ class DataImporterExporter < PluginBase
     print_separator(true)
     
     # Check if the input directory exists
-    if not (File.exists? $INPUT_DIR and File.directory? $INPUT_DIR)
+    if not (File.exist? $INPUT_DIR and File.directory? $INPUT_DIR)
       puts "Input directory #{$INPUT_DIR} does not exist."
       puts "Nothing to import...skipping import."
       puts
@@ -43,7 +43,7 @@ class DataImporterExporter < PluginBase
     end
  
     # Check if the output directory exists
-    if not (File.exists? $OUTPUT_DIR and File.directory? $OUTPUT_DIR)
+    if not (File.exist? $OUTPUT_DIR and File.directory? $OUTPUT_DIR)
       puts "Error: Output directory #{$OUTPUT_DIR} does not exist."
       puts "Hint: Check that the $DATA_DIR variable in paths.rb is set to the correct path."
       puts
